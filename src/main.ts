0662a70b5d7b629c46c7da2ec5925b52039dd6c6
Cristhian Tumbaco <ctumbaco@praxmed.com.ec>
2026-04-28 10:56:24 -0500

    V1.8

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,8 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   const config = new DocumentBuilder()
     .setTitle('Mi API')
-    .setDescription('API Data Conexia 2026-04-23')
-    .setVersion('v1.7')
+    .setDescription('API Data Conexia 2026-04-28')
+    .setVersion('v1.8')
     .addBearerAuth()
     .build();
 
